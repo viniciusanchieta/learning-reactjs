28db598078cdd727971ea90f077540d657ba4c92
Vincius Cesar <contato@viniciuscesar.com>
2020-11-30 23:02:10 -0300

    Finished body of page

--- ui-create/dropbox-menu/src/components/Section/index.tsx
+++ ui-create/dropbox-menu/src/components/Section/index.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const Section: React.FC<Props> = ({variant, title, description}) => {
     return (
-        <Container>
+        <Container className={variant}>
             <Content>
                 <h2>{title}</h2>
                 <p>{description}</p>
